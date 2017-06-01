5b1b994656216bf28a4f7f6a98b72b1772a1cf94
Baka-Yarou <aleemszaidi@gmail.com>
2017-06-01 13:30:47 -0500

    Update BakaPermissions.java

--- BakaPermissions.java
+++ BakaPermissions.java
@@ -2,20 +2,20 @@ package baka;
 
 public class BakaPermissions extends JavaPlugin {
 
-	@Override
-	public void onEnable() {
-		PluginDescriptionFile pdf = this.getDescription();
-		String pluginName = pdf.getName();
-		String pluginVersion = pdf.getVersion();
-		getLogger().info(pluginName + " v" + pluginVersion + " enabled!");
-	}
-	
-	@Override
-	public void onDisable() {
-		PluginDescription pdf = this.getDescription();
-		String pluginName = pdf.getName();
-		String pluginVersion = pdf.getVersion();
-		getLogger().info(pluginName + " v" + pluginVersion + " disabled!");
-	}
+    @Override
+    public void onEnable() {
+        PluginDescriptionFile pdf = this.getDescription();
+        String pluginName = pdf.getName();
+        String pluginVersion = pdf.getVersion();
+        getLogger().info(pluginName + " v" + pluginVersion + " enabled!");
+    }
+    
+    @Override
+    public void onDisable() {
+        PluginDescription pdf = this.getDescription();
+        String pluginName = pdf.getName();
+        String pluginVersion = pdf.getVersion();
+        getLogger().info(pluginName + " v" + pluginVersion + " disabled!");
+    }
 
 }
