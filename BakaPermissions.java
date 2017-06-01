9b8bb2f952a765806d547c473b064090ea14bd2d
Baka-Yarou <aleemszaidi@gmail.com>
2017-05-31 21:56:30 -0500

    Create BakaPermissions.java
    main class

---
+++ BakaPermissions.java
@@ -0,0 +1,21 @@
+package baka.bhai;
+
+public class BakaPermissions extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		PluginDescriptionFile pdf = this.getDescription();
+		String pluginName = pdf.getName();
+		String pluginVersion = pdf.getVersion();
+		getLogger().info(pluginName + " v" + pluginVersion + " enabled!");
+	}
+	
+	@Override
+	public void onDisable() {
+		PluginDescription pdf = this.getDescription();
+		String pluginName = pdf.getName();
+		String pluginVersion = pdf.getVersion();
+		getLogger().info(pluginName + " v" + pluginVersion + " disabled!");
+	}
+
+}
